a8c3b90270b1d275821064953978d6fb43b6e420
hernandezjohnaustine-rgb <hernandezjohnaustine@gmail.com>
2026-07-16 07:28:21 +0800

    add deposits tab button to admin panel

---
+++ gcash5.cjs
@@ -0,0 +1,13 @@
+﻿const fs = require("fs");
+const lines = fs.readFileSync("src/screens/AdminScreen.tsx", "utf8").split("\n");
+
+for (let i = 0; i < lines.length; i++) {
+    if (lines[i].includes('{ id: "gcash", label: "GCash"')) {
+        lines.splice(i, 0, '          { id: "deposits", label: "Deposits", icon: Wallet },');
+        console.log("Added Deposits tab at line", i + 1);
+        break;
+    }
+}
+
+fs.writeFileSync("src/screens/AdminScreen.tsx", lines.join("\n"), "utf8");
+console.log("Done!");
